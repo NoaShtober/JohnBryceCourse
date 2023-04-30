e9f8717a5c34361408ba27872adf5ae3391e063c
NoaShtober <94970175+NoaShtober@users.noreply.github.com>
2023-04-30 14:43:13 +0300

    Create NumsExample.py

---
+++ NumsExample.py
@@ -0,0 +1,18 @@
+num1 = 16
+num2 = 12
+
+sum = num1 + num2
+diff = num1 - num2
+multiple = num1 * num2
+
+print(str(sum))
+print(str(diff))
+print(str(multiple))
+
+if (num1 == 16):
+    print('num1 equals 16')
+
+if (diff == 4):
+    print('true')
+else:
+    print('false')
