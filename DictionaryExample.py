a04238af600f1cfd56ab647d7a253c6edc719bdd
NoaShtober <94970175+NoaShtober@users.noreply.github.com>
2023-04-30 14:45:32 +0300

    Create DictionaryExample.py

---
+++ DictionaryExample.py
@@ -0,0 +1,9 @@
+person = {'first_name': 'john',
+          'last_name': 'Smith',
+          'age': 35,
+          'address': 'Acre'
+          }
+
+first = person['first_name']
+print(first)
+person['age'] = person['age'] + 5
